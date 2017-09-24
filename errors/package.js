d4fcb2b62d403474761ce237d9bf5e051cd56d72
Nguyen Hai Trieu <nguyenhaitrieu10@gmail.com>
2017-09-24 17:21:32 +0700

    version 0.0.1

--- errors/package.js
+++ errors/package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'nghtrieu:errors',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'package support to alert errors',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/nguyenhaitrieu10/nghtrieu-errors',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,13 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.5.2');
-  api.use('ecmascript');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
   api.mainModule('errors.js');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('nghtrieu:errors');
-  api.mainModule('errors-tests.js');
 });
